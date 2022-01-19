08e1927c5b04382af5ea1030ab67d0fd30154943
ThomasWCS <90311824+ThomasWCS@users.noreply.github.com>
2022-01-19 12:42:48 +0100

    Add files via upload

---
+++ src/Security/SecurityAuthenticator.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+
+class SecurityAuthenticator extends AbstractLoginFormAuthenticator
+{
+
+    public const LOGIN_ROUTE = 'login';
+
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    protected function getLoginUrl(\Symfony\Component\HttpFoundation\Request $request): string
+    {
+
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        // TODO: Implement authenticate() method.
+        $email = $request->request->get('_username', '');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($request->request->get('_password', '')),
+            [
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return new RedirectResponse($this->urlGenerator->generate('home'));
+    }
+}
